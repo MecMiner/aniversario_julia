89283587174954a7c64414859c3a3a1109ff8231
MecMiner <souza.jefersoncarlos@gmail.com>
2025-01-29 11:50:38 -0300

    Oi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,122 @@
-import Image from "next/image";
+'use client'
+
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import Head from "next/head";
+import { EffectFade } from "swiper/modules";
+
+const timeline = [
+  { year: "2023", text: "Esse ano foi quando essa pessoa resolveu aparecer na minha vida!", image: "/images/3.jpg" },
+  { year: "2023", text: "Nunca imaginei que depois do nosso primeiro contato iriamos começar a passar tanto tempo juntos!", image: "/images/1.jpg" },
+  { year: "2023", text: "E principalmente nos momentos em que eu mais precisava!", image: "/images/4.jpg" },
+  { year: "2023", text: "Nos divertimos bastante também!", image: "/images/5.jpg" },
+  { year: "2024", text: "Eae começou um outro ano, e você ainda estava lá!", image: "/images/6.jpg" },
+  { year: "2024", text: "E festajamos bastante nesse...", image: "/images/7.jpg" },
+  { year: "2024", text: "Em todas as datas passamos juntos.", image: "/images/8.jpg" },
+  { year: "2025", text: "Nesse ano você conheceu minha família.", image: "/images/12.jpg" },
+  { year: "2025", text: "E mostrou que é uma pilota de fuga", image: "/images/9.jpg" },
+  { year: "2025", text: "Viajamos um pouco mais, e tudo isso me mostrou uma coisa", image: "/images/14.jpg" },
+  { year: "2025", text: "Isso me fez ver o quanto eu sou feliz ao seu lado e que...", image: "/images/13.jpg" },
+  { year: "2025", text: "Quando penso que não posso te amar mais, eu acabo te amando", image: "/images/15.jpg" },
+  { year: "2025", text: 'Feliz Aniversário meu amor! Que todos seus sonhos se realizem! Eu vou está aqui para tentar relizar com você\n TE AMO MIL MILHÕES 💖', image: "/images/fim.jpg" },
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [buttonMusic, setButtonMusic] = useState(true);
+  const [openImage, setOpenImage] = useState<string | null>(null); // Estado para controlar a imagem aberta
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const newAudio = new Audio("/music/love-song.mp3");
+      newAudio.loop = true;
+      setAudio(newAudio);
+      newAudio.play().then(() => {
+        setIsPlaying(true);
+      }).catch(() => {
+        console.log("Autoplay bloqueado, aguardando interação do usuário.");
+      });
+    }
+  }, []);
+
+  const toggleMusic = () => {
+    setButtonMusic(false);
+    if (audio) {
+      if (isPlaying) {
+        audio.pause();
+      } else {
+        audio.play().catch(() => console.log("Autoplay bloqueado!"));
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  const closeImage = () => {
+    setOpenImage(null); // Fecha a imagem quando clicada fora
+  };
+
+  return (
+    <div className="relative flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white overflow-hidden"
+      style={{
+        backgroundImage: "url('/gif/estrelas.gif')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}>
+
+      <div className="absolute inset-0 w-full h-full bg-black opacity-50 z-1" />
+
+      <h1 className="absolute top-8 text-5xl font-bold mt-10 z-10 mb-4 sm:relative sm:top-0">Nossa História 💖</h1>
+
+      <Swiper spaceBetween={20} slidesPerView={1} className="relative w-3/4 sm:w-1/3 mt-10 z-10 items-center justify-center">
+        {timeline.map((item, index) => (
+          <SwiperSlide key={index} className="relative flex flex-col items-center justify-center sm:w-1/3">
+            <div className="w-full overflow-hidden rounded-lg shadow-lg bg-gray-700 flex items-center justify-center aspect-square">
+              <button onClick={() => setOpenImage(item.image)}>
+                <img src={item.image} alt={item.year} className="object-cover w-full h-full" />
+              </button>
+            </div>
+            <h2 className="text-2xl font-semibold mt-4">{item.year}</h2>
+            {item.text.split('\n').map((line, index) => (
+              <span key={index} className="text-lg italic mt-2 text-center">
+                {line}
+                <br/>
+              </span>
+            ))}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <button
+        onClick={toggleMusic}
+        className="absolute bottom-8 sm:relative mt-6 px-6 py-3 bg-pink-500 text-white rounded-lg shadow-lg hover:bg-pink-600 transition z-10 "
+      >
+        {isPlaying ? "Pausar Música ⏸" : "Tocar Música 🎶"}
+      </button>
+      {buttonMusic &&
+        <button
+          onClick={toggleMusic}
+          className="absolute inset-0 w-full h-full opacity-0 z-20"
+        >
+        </button>
+      }
+
+      {/* Modal de imagem */}
+      {openImage && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-30">
+          <div className="relative">
+            <img src={openImage} alt="Imagem Ampliada" className="max-w-xs max-h-3xl object-contain rounded-lg" />
+            <button
+              onClick={closeImage}
+              className="border rounded-full border-gray-800 bg-slate-700 opacity-60 absolute top-2 right-2 text-white font-bold text-2xl p-2"
+            >
+              ✕
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
     </div>
   );
 }
